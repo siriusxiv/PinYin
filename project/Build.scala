5ffdc264e01282fcd57b7dc18f4fbe0438fea5bb
malik <malik@malik-Inspiron-3521.(none)>
2013-08-04 00:27:52 +0200

    favicon

--- project/Build.scala
+++ project/Build.scala
@@ -4,8 +4,8 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "y"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "PinYin"
+  val appVersion      = "1.0"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
