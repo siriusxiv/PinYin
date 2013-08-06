59219e88b0bf6a21fa20c028434ea19f9bef14b5
malik <malik@malik-Inspiron-3521.(none)>
2013-08-07 00:55:35 +0200

    Now multilanguage ! French and English supported

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.util.List;
 
+import langs.Language;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 import sounds.Sounds;
@@ -12,7 +14,17 @@ public class Application extends Controller {
 	
     public static Result index() {
 		List<String> sounds = Sounds.get10();
-        return ok(index.render(sounds));
+		String lang = session("lang");
+		if(Language.exists(lang)){
+	        return ok(index.render(sounds,lang));
+		}else{
+			return ok(index.render(sounds,"en"));
+		}
+    }
+    
+    public static Result changeLanguage(String language){
+    	session("lang",language);
+    	return redirect(routes.Application.index());
     }
   
 }
